a02bee7a8592404c72c91700ce9896e673651318
Abhijith44121 <abhijithabhi508108@gmail.com>
2025-10-08 18:19:29 +0530

    Created a Simple Bg-Changer App using React

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+
+const App = () => {
+  const [color,setColor] = useState("#E1F8DC")
+  return ( 
+   <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+
+    <div className="fixed flex felx-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button 
+        onClick={()=>setColor("red")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+        style={{backgroundColor:"red"}}>Red</button>
+         <button 
+          onClick={()=>setColor("green")}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+        style={{backgroundColor:"green"}}>Green</button>
+         <button 
+          onClick={()=>setColor("blue")}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+        style={{backgroundColor:"blue"}}>Blue</button>
+        <button 
+          onClick={()=>setColor("maroon")}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+        style={{backgroundColor:"maroon"}}>Maroon</button>
+        <button 
+          onClick={()=>setColor("purple")}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+        style={{backgroundColor:"purple"}}>Purple</button>
+        <button 
+          onClick={()=>setColor("black")}
+         className="outline-none px-4 py-1 rounded-full text-white shadow-lg cursor-pointer"
+        style={{backgroundColor:"black"}}>Black</button>
+
+
+      </div>
+    </div>
+   </div>
+   );
+}
+ 
+export default App;
